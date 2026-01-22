53d9820e9aa32fd588ddb1b751e8f9976626460b
Kaizen-246 <kc101@kent.ac.uk>
2026-01-22 09:33:56 +0000

    final

--- partOne.py
+++ partOne.py
@@ -3,6 +3,7 @@ def main():
     myFunction(slow)
 
 def myFunction(text):
-  #Your code goes here.
+  slowed = text.replace(" ", "...")
+  print(slowed)
 
 main()
--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,14 @@
 import math  
 
 def main():
-#TO DO  
+    A = int(input("Enter A: "))
+    B = int(input("Enter B: "))
+    c = pythag (A, B)
+    print (c) 
 
 def pythag(A,B):
-#TO DO  
+   c=math.sqrt ((A**2)+(B**2))
+   return c
+
 
 main()
